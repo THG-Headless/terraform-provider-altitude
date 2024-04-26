ccc062f981846ec4eb08f36fe439fe88dc67214a
Gulfam Mahmud <gulfam.mahmud29@thehutgroup.com>
2024-04-26 10:19:14 +0100

    created a transformer wip

--- examples/provider-verification/main.tf
+++ examples/provider-verification/main.tf
@@ -14,11 +14,19 @@ provider "altitude" {
 resource "altitude_mte_config" "item_1" {
   routes = [
     {
-      host = "yo"
-      path = "yo"
-      enable_ssl = true
+      host                 = "yo"
+      path                 = "yo"
+      enable_ssl           = true
       preserve_path_prefix = true
-      shield_location = "New York City"
+      shield_location      = "New York City"
     }
   ]
+
+  basic_auth = {
+    username = "yo"
+    password = "yo"
+  }
+
 }
+
+
--- internal/provider/config_resource.go
+++ internal/provider/config_resource.go
@@ -2,6 +2,7 @@ package provider
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
@@ -26,6 +27,11 @@ type MTEConfigResource struct {
 }
 
 type MTEConfigResourceModel struct {
+	EnvironmentId types.String       `tfsdk:"environment_id"`
+	Config        MTEConfigBodyModel `tfsdk:"config"`
+}
+
+type MTEConfigBodyModel struct {
 	Routes    []RouteModel   `tfsdk:"routes"`
 	BasicAuth BasicAuthModel `tfsdk:"basic_auth"`
 }
@@ -69,6 +75,33 @@ type BasicAuthModel struct {
 	Password types.String `tfsdk:"password"`
 }
 
+// Post Request Body
+
+type HTTPPostRequestBody struct {
+	Routes    RoutesPostBody
+	BasicAuth BasicAuthPostBody
+}
+
+type BasicAuthPostBody struct {
+	username string
+	password string
+}
+
+type RoutesPostBody struct {
+	host               string
+	path               string
+	enableSsl          bool
+	preservePathPrefix bool
+	cacheKey           CacheKeyModel
+	appendPathPrefix   string
+	shieldLocation     ShieldLocation
+}
+
+type CacheKeyRequestBodyModel struct {
+	header string
+	cookie string
+}
+
 // Metadata implements resource.Resource.
 func (m *MTEConfigResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
 	resp.TypeName = req.ProviderTypeName + "_mte_config"
@@ -80,68 +113,90 @@ func (m *MTEConfigResource) Schema(ctx context.Context, req resource.SchemaReque
 		MarkdownDescription: "Config lock to enable mte",
 
 		Attributes: map[string]schema.Attribute{
-			"routes": schema.ListNestedAttribute{
+			"config": schema.SingleNestedAttribute{
 				Required: true,
-				NestedObject: schema.NestedAttributeObject{
-					Attributes: map[string]schema.Attribute{
-						"host": schema.StringAttribute{
-							Required:            true,
-							MarkdownDescription: "yo",
-						},
-						"path": schema.StringAttribute{
-							Required:            true,
-							MarkdownDescription: "yo",
-						},
-						"enable_ssl": schema.BoolAttribute{
-							Required:            true,
-							MarkdownDescription: "yo",
-						},
-						"preserve_path_prefix": schema.BoolAttribute{
-							Required:            true,
-							MarkdownDescription: "yo",
-						},
-						"cache_key": schema.SingleNestedAttribute{
-							Optional: true,
+				Attributes: map[string]schema.Attribute{
+					"routes": schema.ListNestedAttribute{
+						Required: true,
+						NestedObject: schema.NestedAttributeObject{
 							Attributes: map[string]schema.Attribute{
-								"headers": schema.ListAttribute{
-									ElementType: types.StringType,
-									Required:    true,
+								"host": schema.StringAttribute{
+									Required:            true,
+									MarkdownDescription: "yo",
 								},
-								"cookies": schema.ListAttribute{
-									ElementType: types.StringType,
-									Required:    true,
+								"path": schema.StringAttribute{
+									Required:            true,
+									MarkdownDescription: "yo",
+								},
+								"enable_ssl": schema.BoolAttribute{
+									Required:            true,
+									MarkdownDescription: "yo",
+								},
+								"preserve_path_prefix": schema.BoolAttribute{
+									Required:            true,
+									MarkdownDescription: "yo",
+								},
+								"cache_key": schema.SingleNestedAttribute{
+									Optional: true,
+									Attributes: map[string]schema.Attribute{
+										"headers": schema.ListAttribute{
+											ElementType: types.StringType,
+											Required:    true,
+										},
+										"cookies": schema.ListAttribute{
+											ElementType: types.StringType,
+											Required:    true,
+										},
+									},
+									MarkdownDescription: "yo",
+								},
+								"append_path_prefix": schema.StringAttribute{
+									Optional:            true,
+									MarkdownDescription: "yo",
+								},
+								"shield_location": schema.StringAttribute{
+									Optional:            true,
+									MarkdownDescription: "yo",
+									Validators: []validator.String{
+										stringvalidator.OneOf([]string{string(London),
+											string(Manchester),
+											string(New_York_City),
+											string(Frankfurt),
+											string(Madrid),
+											string(Los_Angeles),
+											string(Toronto),
+											string(Johannesburg),
+											string(Seoul),
+											string(Sydney),
+											string(Tokyo),
+											string(Hong_Kong),
+											string(Mumbai),
+											string(Singapore),
+										}...,
+										),
+									},
 								},
 							},
-							MarkdownDescription: "yo",
 						},
-						"append_path_prefix": schema.StringAttribute{
-							Optional:            true,
-							MarkdownDescription: "yo",
-						},
-						"shield_location": schema.StringAttribute{
-							Optional:            true,
-							MarkdownDescription: "yo",
-							Validators: []validator.String{
-								stringvalidator.OneOf([]string{string(London),
-									string(Manchester),
-									string(New_York_City),
-									string(Frankfurt),
-									string(Madrid),
-									string(Los_Angeles),
-									string(Toronto),
-									string(Johannesburg),
-									string(Seoul),
-									string(Sydney),
-									string(Tokyo),
-									string(Hong_Kong),
-									string(Mumbai),
-									string(Singapore),
-								}...,
-								),
+					},
+					"basic_auth": schema.SingleNestedAttribute{
+						Optional: true,
+						Attributes: map[string]schema.Attribute{
+							"username": schema.StringAttribute{
+								Required:            true,
+								MarkdownDescription: "yo",
+							},
+							"password": schema.StringAttribute{
+								Required:            true,
+								MarkdownDescription: "yo",
 							},
 						},
 					},
 				},
+			},
+			"environment_id": schema.StringAttribute{
+				Required:            true,
+				MarkdownDescription: "yo",
 			},
 		},
 	}
@@ -153,8 +208,31 @@ func (m *MTEConfigResource) ImportState(context.Context, resource.ImportStateReq
 }
 
 // Create implements resource.Resource.
-func (m *MTEConfigResource) Create(context.Context, resource.CreateRequest, *resource.CreateResponse) {
-	panic("unimplemented")
+func (m *MTEConfigResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var data MTEConfigResourceModel
+
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	err := m.createMteConfig(
+		ctx,
+		data,
+	)
+
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Failed to create MTE config",
+			"An error occurred while executing the creation. "+
+				"If unexpected, please report this issue to the provider developers.\n\n"+
+				"JSON Error: "+err.Error())
+		return
+	}
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+
 }
 
 // Delete implements resource.Resource.
@@ -171,3 +249,51 @@ func (m *MTEConfigResource) Read(context.Context, resource.ReadRequest, *resourc
 func (m *MTEConfigResource) Update(context.Context, resource.UpdateRequest, *resource.UpdateResponse) {
 	panic("unimplemented")
 }
+
+func (m *MTEConfigResource) createMteConfig(
+	ctx context.Context,
+	data MTEConfigResourceModel,
+) error {
+	httpReq, err := http.NewRequest(
+		http.MethodPost,
+		fmt.Sprintf("%s/v1/environment/%s/mte/altitude-config", m.baseUrl, data.EnvironmentId.ValueString()),
+		nil,
+	)
+}
+
+func (m *MTEConfigResourceModel) transformToHttpPostBody(
+	data MTEConfigResourceModel,
+) HTTPPostRequestBody {
+
+	var httpRoutes = []RoutesPostBody{}
+	for i := 0; i < len(data.Config.Routes); i++ {
+		var r = data.Config.Routes[i]
+		var password = data.Config.BasicAuth.Password
+		var username = data.Config.BasicAuth.Username
+		var basicAuthPostBody = BasicAuthPostBody{
+			username: username.ValueString(),
+			password: password.ValueString(),
+		}
+		var routesPostBody = RoutesPostBody{
+			host:               r.host.ValueString(),
+			path:               r.path.ValueString(),
+			enableSsl:          r.enableSsl.ValueBool(),
+			preservePathPrefix: r.preservePathPrefix.ValueBool(),
+			appendPathPrefix:   r.appendPathPrefix.ValueString(),
+		}
+
+		//this was done quickly not too sure
+
+		var postRequestBody = HTTPPostRequestBody{
+			RoutesPostBody:    routesPostBody,
+			BasicAuthPostBody: basicAuthPostBody,
+		}
+
+		httpRoutes = append(httpRoutes, routesPostBody)
+	}
+
+}
+
+//http request = postrequestbody + basic auth
+
+//transform mte config resource model into http body
