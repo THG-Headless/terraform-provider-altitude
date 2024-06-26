b133024693d66cdeb05131da4b343433643ca1b7
Eleanor Kilgallon <eleanor.kilgallon78@thehutgroup.com>
2024-06-26 16:15:36 +0100

    usign correct commands

--- internal/provider/data_logging_endpoints_test.go
+++ internal/provider/data_logging_endpoints_test.go
@@ -16,9 +16,9 @@ func TestAccLoggingEndpointsDataSource(t *testing.T) {
 				Config: `data "altitude_mte_logging_endpoints" "test" {}`,
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttrSet("data.altitude_mte_logging_endpoints.test", "endpoints.0.type"),
-					resource.TestCheckResourceAttr("data.altitude_mte_logging_endpoints.test", "endpoints.0.environmentid"),
-					resource.TestCheckResourceAttr("data.altitude_mte_logging_endpoints.test", "endpoints.0.config"),
-					resource.TestCheckResourceAttr("data.altitude_mte_logging_endpoints.test", "endpoints.0.id"),
+					resource.TestCheckResourceAttrSet("data.altitude_mte_logging_endpoints.test", "endpoints.0.environmentid"),
+					resource.TestCheckResourceAttrSet("data.altitude_mte_logging_endpoints.test", "endpoints.0.config"),
+					resource.TestCheckResourceAttrSet("data.altitude_mte_logging_endpoints.test", "endpoints.0.id"),
 				),
 			},
 		},
