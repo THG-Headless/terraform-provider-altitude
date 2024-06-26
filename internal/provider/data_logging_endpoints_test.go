3fc912086c1a1bd721c5b703ca6e6cc6c0780bc7
Eleanor Kilgallon <eleanor.kilgallon78@thehutgroup.com>
2024-06-26 16:14:31 +0100

    removing unused types

--- internal/provider/data_logging_endpoints_test.go
+++ internal/provider/data_logging_endpoints_test.go
@@ -6,20 +6,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
-type GetAbstractAccessLoggingConfigModel struct {
-	Dataset   types.String           `tfsdk:"dataset"`
-	ProjectId types.String           `tfsdk:"projectid"`
-	Table     types.String           `tfsdk:"table"`
-	Email     types.String           `tfsdk:"email"`
-	Headers   []BqLoggingHeaderModel `tfsdk:"headers"`
-	SecretKey types.String           `tfsdk:"secretkey"`
-}
-
-type BqLoggingHeaderModel struct {
-	ColumnName   types.String `tfsdk:"columnname"`
-	HeaderName   types.String `tfsdk:"headername"`
-	DefaultValue types.String `tfsdk:"defaultvalue"`
-}
 
 func TestAccLoggingEndpointsDataSource(t *testing.T) {
 	resource.Test(t, resource.TestCase{
