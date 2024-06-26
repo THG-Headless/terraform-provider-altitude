8566bdb4ae4487f6d0d68027f8b1d13b86b712fc
Eleanor Kilgallon <eleanor.kilgallon78@thehutgroup.com>
2024-06-26 16:13:00 +0100

    adding comma

--- internal/provider/data_logging_endpoints_test.go
+++ internal/provider/data_logging_endpoints_test.go
@@ -29,7 +29,7 @@ func TestAccLoggingEndpointsDataSource(t *testing.T) {
 			{
 				Config: `data "altitude_mte_logging_endpoints" "test" {}`,
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttrSet("data.altitude_mte_logging_endpoints.test", "endpoints.0.type")
+					resource.TestCheckResourceAttrSet("data.altitude_mte_logging_endpoints.test", "endpoints.0.type"),
 					resource.TestCheckResourceAttr("data.altitude_mte_logging_endpoints.test", "endpoints.0.environmentid"),
 					resource.TestCheckResourceAttr("data.altitude_mte_logging_endpoints.test", "endpoints.0.config"),
 					resource.TestCheckResourceAttr("data.altitude_mte_logging_endpoints.test", "endpoints.0.id"),
