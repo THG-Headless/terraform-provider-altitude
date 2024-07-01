2ac2dcb99f5b2e92590635045a1e56bc3e230c51
Eleanor Kilgallon <eleanor.kilgallon78@thehutgroup.com>
2024-07-01 15:31:35 +0100

    fixing build

--- internal/provider/data_logging_endpoints_test.go
+++ internal/provider/data_logging_endpoints_test.go
@@ -21,7 +21,6 @@ func TestAccLoggingEndpointsDataSource(t *testing.T) {
 					resource.TestCheckResourceAttrSet("data.altitude_mte_logging_endpoints.test", "endpoints.0.config.table"),
 					resource.TestCheckResourceAttrSet("data.altitude_mte_logging_endpoints.test", "endpoints.0.config.email"),
 					resource.TestCheckResourceAttrSet("data.altitude_mte_logging_endpoints.test", "endpoints.0.config.secretkey"),
-					resource.TestCheckResourceAttrSet("data.altitude_mte_logging_endpoints.test", "endpoints.0.id"),
 				),
 			},
 		},
